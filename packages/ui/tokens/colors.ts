59f1b5239aeb9dbdb5ad5f9cc342e4abddee7214
aqing02 <1134575174@qq.com>
2024-02-24 11:45:11 +0800

    修改主题色

--- packages/ui/tokens/colors.ts
+++ packages/ui/tokens/colors.ts
@@ -1,90 +1,270 @@
+// export const baseColors = {
+//   white: 'white',
+//   failure: '#ED4B9E',
+//   failure33: '#ED4B9E33',
+//   primary: 'rgba(47, 138, 245, 1)',
+//   primary_btn: 'rgba(47, 138, 245, .2)',
+//   primary_old: '#1FC7D4',
+//   primary0f: '#1FC7D40f',
+//   primary3D: '#1FC7D43D',
+//   primaryBright: '#53DEE9',
+//   primaryDark: '#0098A1',
+//   success: '#31D0AA',
+//   success19: '#31D0AA19',
+//   warning: '#FFB237',
+//   warning2D: '#ED4B9E2D',
+//   warning33: '#ED4B9E33',
+// }
+
+// export const additionalColors = {
+//   binance: '#F0B90B',
+//   overlay: '#452a7a',
+//   gold: '#FFC700',
+//   silver: '#B2B2B2',
+//   bronze: '#E7974D',
+// }
+
+// export const lightColors = {
+//   ...baseColors,
+//   ...additionalColors,
+//   secondary: '#2f8af5',
+//   secondary80: '#7645D980',
+//   background: '#FAF9FA',
+//   backgroundDisabled: '#E9EAEB',
+//   backgroundAlt: '#FFFFFF',
+//   backgroundAlt2: 'rgba(255, 255, 255, 0.7)',
+//   cardBorder: '#E7E3EB',
+//   contrast: '#191326',
+//   dropdown: '#F6F6F6',
+//   dropdownDeep: '#EEEEEE',
+//   invertedContrast: '#2f8af5',
+//   input: '#eeeaf4',
+//   inputSecondary: '#d7caec',
+//   tertiary: '#EFF4F5',
+//   text: '#280D5F',
+//   text99: '#280D5F99',
+//   textDisabled: '#BDC2C4',
+//   textSubtle: '#7A6EAA',
+//   disabled: '#E9EAEB',
+//   gradientBubblegum: 'linear-gradient(139.73deg, #E5FDFF 0%, #F3EFFF 100%)',
+//   gradientInverseBubblegum: 'linear-gradient(139.73deg, #F3EFFF 0%, #E5FDFF 100%)',
+//   gradientCardHeader: 'linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)',
+//   gradientBlue: 'linear-gradient(180deg, #A7E8F1 0%, #94E1F2 100%)',
+//   gradientViolet: 'linear-gradient(180deg, #E2C9FB 0%, #CDB8FA 100%)',
+//   gradientVioletAlt: 'linear-gradient(180deg, #CBD7EF 0%, #9A9FD0 100%)',
+//   gradientGold: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)',
+//   gradientBold: 'linear-gradient(#53DEE9, #7645D9)',
+// }
+
+// export const darkColors = {
+//   ...baseColors,
+//   ...additionalColors,
+//   secondary: '#9A6AFF',
+//   secondary80: '#9A6AFF80',
+//   background: '#08060B',
+//   backgroundDisabled: '#3c3742',
+//   backgroundAlt: '#27262c',
+//   backgroundAlt2: 'rgba(39, 38, 44, 0.7)',
+//   cardBorder: '#383241',
+//   contrast: '#FFFFFF',
+//   dropdown: '#1E1D20',
+//   dropdownDeep: '#100C18',
+//   invertedContrast: '#191326',
+//   input: '#372F47',
+//   inputSecondary: '#262130',
+//   primaryDark: '#0098A1',
+//   tertiary: '#353547',
+//   text: '#F4EEFF',
+//   text99: '#F4EEFF99',
+//   textDisabled: '#666171',
+//   textSubtle: '#B8ADD2',
+//   disabled: '#524B63',
+//   gradientBubblegum: 'linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)',
+//   gradientInverseBubblegum: 'linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)',
+//   gradientCardHeader: 'linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)',
+//   gradientBlue: 'linear-gradient(180deg, #00707F 0%, #19778C 100%)',
+//   gradientViolet: 'linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)',
+//   gradientVioletAlt: 'linear-gradient(180deg, #434575 0%, #66578D 100%)',
+//   gradientGold: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)',
+//   gradientBold: 'linear-gradient(#53DEE9, #9A6AFF)',
+// }
+// export const baseColors = {
+//   white: 'white',
+//   failure: '#ED4B9E',
+//   failure33: '#ED4B9E33',
+//   primary: 'rgba(47, 138, 245, 1)',
+//   primary_btn: 'rgba(47, 138, 245, .2)',
+//   primary_old: '#1FC7D4',
+//   primary0f: '#1FC7D40f',
+//   primary3D: '#1FC7D43D',
+//   primaryBright: '#53DEE9',
+//   primaryDark: '#0098A1',
+//   success: '#31D0AA',
+//   success19: '#31D0AA19',
+//   warning: '#FFB237',
+//   warning2D: '#ED4B9E2D',
+//   warning33: '#ED4B9E33',
+// }
+
+// export const additionalColors = {
+//   binance: '#F0B90B',
+//   overlay: '#452a7a',
+//   gold: '#FFC700',
+//   silver: '#B2B2B2',
+//   bronze: '#E7974D',
+// }
+
+// export const lightColors = {
+//   ...baseColors,
+//   ...additionalColors,
+//   secondary: '#2f8af5',
+//   secondary80: '#7645D980',
+//   background: '#FAF9FA',
+//   backgroundDisabled: '#E9EAEB',
+//   backgroundAlt: '#FFFFFF',
+//   backgroundAlt2: 'rgba(255, 255, 255, 0.7)',
+//   cardBorder: '#E7E3EB',
+//   contrast: '#191326',
+//   dropdown: '#F6F6F6',
+//   dropdownDeep: '#EEEEEE',
+//   invertedContrast: '#2f8af5',
+//   input: '#eeeaf4',
+//   inputSecondary: '#d7caec',
+//   tertiary: '#EFF4F5',
+//   text: '#280D5F',
+//   text99: '#280D5F99',
+//   textDisabled: '#BDC2C4',
+//   textSubtle: '#7A6EAA',
+//   disabled: '#E9EAEB',
+//   gradientBubblegum: 'linear-gradient(139.73deg, #E5FDFF 0%, #F3EFFF 100%)',
+//   gradientInverseBubblegum: 'linear-gradient(139.73deg, #F3EFFF 0%, #E5FDFF 100%)',
+//   gradientCardHeader: 'linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)',
+//   gradientBlue: 'linear-gradient(180deg, #A7E8F1 0%, #94E1F2 100%)',
+//   gradientViolet: 'linear-gradient(180deg, #E2C9FB 0%, #CDB8FA 100%)',
+//   gradientVioletAlt: 'linear-gradient(180deg, #CBD7EF 0%, #9A9FD0 100%)',
+//   gradientGold: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)',
+//   gradientBold: 'linear-gradient(#53DEE9, #7645D9)',
+// }
+
+// export const darkColors = {
+//   ...baseColors,
+//   ...additionalColors,
+//   secondary: '#9A6AFF',
+//   secondary80: '#9A6AFF80',
+//   background: '#08060B',
+//   backgroundDisabled: '#3c3742',
+//   backgroundAlt: '#27262c',
+//   backgroundAlt2: 'rgba(39, 38, 44, 0.7)',
+//   cardBorder: '#383241',
+//   contrast: '#FFFFFF',
+//   dropdown: '#1E1D20',
+//   dropdownDeep: '#100C18',
+//   invertedContrast: '#191326',
+//   input: '#372F47',
+//   inputSecondary: '#262130',
+//   primaryDark: '#0098A1',
+//   tertiary: '#353547',
+//   text: '#F4EEFF',
+//   text99: '#F4EEFF99',
+//   textDisabled: '#666171',
+//   textSubtle: '#B8ADD2',
+//   disabled: '#524B63',
+//   gradientBubblegum: 'linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)',
+//   gradientInverseBubblegum: 'linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)',
+//   gradientCardHeader: 'linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)',
+//   gradientBlue: 'linear-gradient(180deg, #00707F 0%, #19778C 100%)',
+//   gradientViolet: 'linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)',
+//   gradientVioletAlt: 'linear-gradient(180deg, #434575 0%, #66578D 100%)',
+//   gradientGold: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)',
+//   gradientBold: 'linear-gradient(#53DEE9, #9A6AFF)',
+// }
 export const baseColors = {
   white: 'white',
-  failure: '#ED4B9E',
-  failure33: '#ED4B9E33',
-  primary: 'rgba(47, 138, 245, 1)',
-  primary_btn: 'rgba(47, 138, 245, .2)',
-  primary_old: '#1FC7D4',
-  primary0f: '#1FC7D40f',
-  primary3D: '#1FC7D43D',
-  primaryBright: '#53DEE9',
-  primaryDark: '#0098A1',
-  success: '#31D0AA',
-  success19: '#31D0AA19',
-  warning: '#FFB237',
-  warning2D: '#ED4B9E2D',
-  warning33: '#ED4B9E33',
+  failure: '#ffa500', // 更改为红色
+  failure33: '#ffa50033', // 更改为带透明度的红色
+  primary: '#ffa500', // 更改为红色
+  primary_btn: 'rgba(255,162,7, .2)', // 更改为带透明度的红色
+  primary_old: '#00FF00', // 更改为绿色
+  primary0f: '#00FF000f', // 更改为带透明度的绿色
+  primary3D: '#00FF003D', // 更改为带透明度的绿色
+  primaryBright: '#FFFF00', // 更改为黄色
+  primaryDark: '#0000FF', // 更改为蓝色
+  success: '#00FF00', // 更改为绿色
+  success19: '#00FF0019', // 更改为带透明度的绿色
+  warning: '#FFA500', // 更改为橙色
+  warning2D: '#FFA5002D', // 更改为带透明度的橙色
+  warning33: '#FFA50033', // 更改为带透明度的橙色
 }
 
 export const additionalColors = {
-  binance: '#F0B90B',
-  overlay: '#452a7a',
-  gold: '#FFC700',
-  silver: '#B2B2B2',
-  bronze: '#E7974D',
+  binance: '#FFFF00', // 更改为黄色
+  overlay: '#800080', // 更改为紫色
+  gold: '#FFD700', // 更改为金色
+  silver: '#C0C0C0', // 更改为银色
+  bronze: '#CD7F32', // 更改为青铜色
 }
 
 export const lightColors = {
   ...baseColors,
   ...additionalColors,
-  secondary: '#2f8af5',
-  secondary80: '#7645D980',
-  background: '#FAF9FA',
-  backgroundDisabled: '#E9EAEB',
-  backgroundAlt: '#FFFFFF',
-  backgroundAlt2: 'rgba(255, 255, 255, 0.7)',
-  cardBorder: '#E7E3EB',
-  contrast: '#191326',
-  dropdown: '#F6F6F6',
-  dropdownDeep: '#EEEEEE',
-  invertedContrast: '#2f8af5',
-  input: '#eeeaf4',
-  inputSecondary: '#d7caec',
-  tertiary: '#EFF4F5',
-  text: '#280D5F',
-  text99: '#280D5F99',
-  textDisabled: '#BDC2C4',
-  textSubtle: '#7A6EAA',
-  disabled: '#E9EAEB',
-  gradientBubblegum: 'linear-gradient(139.73deg, #E5FDFF 0%, #F3EFFF 100%)',
-  gradientInverseBubblegum: 'linear-gradient(139.73deg, #F3EFFF 0%, #E5FDFF 100%)',
-  gradientCardHeader: 'linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)',
-  gradientBlue: 'linear-gradient(180deg, #A7E8F1 0%, #94E1F2 100%)',
-  gradientViolet: 'linear-gradient(180deg, #E2C9FB 0%, #CDB8FA 100%)',
-  gradientVioletAlt: 'linear-gradient(180deg, #CBD7EF 0%, #9A9FD0 100%)',
-  gradientGold: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)',
-  gradientBold: 'linear-gradient(#53DEE9, #7645D9)',
-}
-
-export const darkColors = {
-  ...baseColors,
-  ...additionalColors,
-  secondary: '#9A6AFF',
-  secondary80: '#9A6AFF80',
-  background: '#08060B',
-  backgroundDisabled: '#3c3742',
-  backgroundAlt: '#27262c',
-  backgroundAlt2: 'rgba(39, 38, 44, 0.7)',
-  cardBorder: '#383241',
-  contrast: '#FFFFFF',
-  dropdown: '#1E1D20',
-  dropdownDeep: '#100C18',
-  invertedContrast: '#191326',
-  input: '#372F47',
-  inputSecondary: '#262130',
-  primaryDark: '#0098A1',
-  tertiary: '#353547',
-  text: '#F4EEFF',
-  text99: '#F4EEFF99',
-  textDisabled: '#666171',
-  textSubtle: '#B8ADD2',
-  disabled: '#524B63',
-  gradientBubblegum: 'linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)',
-  gradientInverseBubblegum: 'linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)',
+  secondary: '#FFA500', // 更改为橙色
+  secondary80: '#FFA50080', // 更改为带透明度的橙色
+  background: '#FFFFFF', // 更改为白色
+  backgroundDisabled: '#D3D3D3', // 更改为浅灰色
+  backgroundAlt: '#FFFFE0', // 更改为浅黄色
+  backgroundAlt2: 'rgba(255, 255, 224, 0.7)', // 更改为带透明度的浅黄色
+  cardBorder: '#D3D3D3', // 更改为浅灰色
+  contrast: '#000000', // 更改为黑色
+  dropdown: '#F5F5DC', // 更改为米黄色
+  dropdownDeep: '#FFFFE0', // 更改为深浅黄色
+  invertedContrast: '#FFA500', // 更改为橙色
+  input: '#F0FFF0', // 更改为薄荷绿
+  inputSecondary: '#D3D3D3', // 更改为浅灰色
+  tertiary: '#F5F5DC', // 更改为米黄色
+  text: '#280D5F', // 更改为紫色
+  text99: '#280D5F99', // 更改为带透明度的紫色
+  textDisabled: '#A9A9A9', // 更改为深灰色
+  textSubtle: '#9370DB', // 更改为紫罗兰色
+  disabled: '#D3D3D3', // 更改为浅灰色
+  // gradientBubblegum: 'linear-gradient(139.73deg, #FF1493 0%, #FF69B4 100%)', // 更改为线性渐变的粉红色
+  gradientBubblegum: '#f4f5f6', // 更改为线性渐变的粉红色
+  gradientInverseBubblegum: '#f4f5f6', // 更改为线性渐变的粉红色
   gradientCardHeader: 'linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)',
   gradientBlue: 'linear-gradient(180deg, #00707F 0%, #19778C 100%)',
   gradientViolet: 'linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)',
   gradientVioletAlt: 'linear-gradient(180deg, #434575 0%, #66578D 100%)',
   gradientGold: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)',
-  gradientBold: 'linear-gradient(#53DEE9, #9A6AFF)',
+  gradientBold: 'linear-gradient(#53DEE9, #7645D9)'
+}
+export const darkColors = {
+  ...baseColors,
+    ...additionalColors,
+    secondary: '#9A6AFF',
+    secondary80: '#9A6AFF80',
+    background: '#08060B',
+    backgroundDisabled: '#3c3742',
+    backgroundAlt: '#27262c',
+    backgroundAlt2: 'rgba(39, 38, 44, 0.7)',
+    cardBorder: '#383241',
+    contrast: '#FFFFFF',
+    dropdown: '#1E1D20',
+    dropdownDeep: '#100C18',
+    invertedContrast: '#191326',
+    input: '#372F47',
+    inputSecondary: '#262130',
+    primaryDark: '#0098A1',
+    tertiary: '#353547',
+    text: '#F4EEFF',
+    text99: '#F4EEFF99',
+    textDisabled: '#666171',
+    textSubtle: '#B8ADD2',
+    disabled: '#524B63',
+    gradientBubblegum: 'linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)',
+    gradientInverseBubblegum: 'linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)',
+    gradientCardHeader: 'linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)',
+    gradientBlue: 'linear-gradient(180deg, #00707F 0%, #19778C 100%)',
+    gradientViolet: 'linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)',
+    gradientVioletAlt: 'linear-gradient(180deg, #434575 0%, #66578D 100%)',
+    gradientGold: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)',
+    gradientBold: 'linear-gradient(#53DEE9, #9A6AFF)',
 }
